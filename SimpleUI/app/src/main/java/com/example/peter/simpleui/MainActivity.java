b9036ec749766bcc11a2ff2757af54df4d27bd6c
LUYP <102306040@nccu.edu.tw>
2016-04-25 21:55:38 +0800

    Add listView and order adapter and spinner and array.xml

--- SimpleUI/app/src/main/java/com/example/peter/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/peter/simpleui/MainActivity.java
@@ -5,20 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     EditText editText;
     RadioGroup radioGroup;
+    ArrayList<Order> orders;
     String drinkName = "Black tea";
     CheckBox checkBox;
+    ListView listView;
     String note = "";
+    Spinner spinner;
 
 
 
@@ -31,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.editText);
         radioGroup = (RadioGroup)findViewById(R.id.firstRadioGroup);
         checkBox = (CheckBox)findViewById(R.id.hideCheckBox);
+        listView = (ListView)findViewById(R.id.listView);
+        spinner = (Spinner)findViewById(R.id.spinner);
+        orders = new ArrayList<>();
 
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
@@ -57,18 +68,42 @@ public class MainActivity extends AppCompatActivity {
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton = (RadioButton)findViewById(checkedId);
+                RadioButton radioButton = (RadioButton) findViewById(checkedId);
                 drinkName = radioButton.getText().toString();
             }
         });
 
+        setupListView();
+        setupSpinner();
+
+    }
+
+    void setupListView(){
+        OrderAdapter adapter = new OrderAdapter(this, orders);
+        listView.setAdapter(adapter);
+    }
+
+    void setupSpinner(){
+        String[] data = getResources().getStringArray(R.array.storeInfo);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, data);
+
+        spinner.setAdapter(adapter);
     }
 
     public void click(View view){
         note = editText.getText().toString();
         String text = note;
         textView.setText(text);
+        Order order = new Order();
+        order.drinkName = drinkName;
+        order.note = note;
+        order.storeInfo = (String)spinner.getSelectedItem();
+
+        orders.add(order);
+
         editText.setText("");
+
+        setupListView();
 
     }
 
---
+++ SimpleUI/app/src/main/java/com/example/peter/simpleui/Order.java
@@ -0,0 +1,10 @@
+package com.example.peter.simpleui;
+
+/**
+ * Created by Peter on 2016/4/25.
+ */
+public class Order {
+    String note;
+    String drinkName;
+    String storeInfo;
+}
